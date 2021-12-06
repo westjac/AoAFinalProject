b8ea35fc552214c29ba2f6c283881542c659f33a
Jacob West <jwest@omnitech-inc.com>
2021-12-06 12:22:59 -0700

    Rearanged the function to be more testable

--- kmp/scripts/KMP.js
+++ kmp/scripts/KMP.js
@@ -1,3 +1,4 @@
+// start of KMP algorithm
 export function KMP(pattern, text) {
     let M = pattern.length;
     let N = text.length;
@@ -5,46 +6,52 @@ export function KMP(pattern, text) {
     let j = 0;
     let i = 0;
     let calculatedLps = CalculateLPSArray(pattern, M, lps);
-    
+    let indexLocations = [];
+    //let numberOfOccurences = 0;
+  
     while (i < N) {
-        if (pattern[j] == text[i]) {
-            i += 1;
-            j += 1;
+      if (pattern[j] == text[i]) {
+        i += 1;
+        j += 1;
+      }
+  
+      if (j == M) {
+        indexLocations.push(i - j); // add index of match
+        //console.log("Found pattern at index " + (i - j));
+        //numberOfOccurences += 1;
+        j = calculatedLps[j - 1];
+      } else if (i < N && pattern[j] != text[i]) {
+        if (j != 0) {
+          j = calculatedLps[j - 1];
+        } else {
+          i += 1;
         }
-
-        if (j == M) {
-            console.log("Found pattern at index " + (i - j));
-            j = calculatedLps[j-1];
-        }
-        else if ((i < N) && (pattern[j] != text[i])) {
-            if (j != 0) {
-                j = calculatedLps[j-1];
-            } else {
-                i += 1;
-            }
-        }
+      }
     }
-}
-
-function CalculateLPSArray(pattern, M, lps) {
+  
+    //return numberOfOccurences;
+    return indexLocations;
+  }
+  
+  function CalculateLPSArray(pattern, M, lps) {
     let size = 0;
     let i = 1;
-
+  
     while (i < M) {
-        if(pattern[i] == pattern[size]) {
-            size += 1;
-            lps[i] = size;
-            i += 1;
+      if (pattern[i] == pattern[size]) {
+        size += 1;
+        lps[i] = size;
+        i += 1;
+      } else {
+        if (size != 0) {
+          size = lps[size - 1];
         } else {
-            if (size != 0) {
-                size = lps[size-1];
-            } else {
-                lps[i] = 0;
-                i += 1;
-            }
+          lps[i] = 0;
+          i += 1;
         }
+      }
     }
-
+  
     return lps;
-}
+  }
 
--- kmp/scripts/app.js
+++ kmp/scripts/app.js
@@ -1,3 +1,5 @@
+import { KMP } from "./KMP";
+
 export async function GetRedditPosts(patternToMatch) {
   var posts = "";
 
@@ -8,11 +10,11 @@ export async function GetRedditPosts(patternToMatch) {
 
   console.log("Running KMP...");
 
-  var numberOfMatches = KMP(patternToMatch, posts);
+  var matchLocations = KMP(patternToMatch, posts);
 
-  console.log("Matches from KMP: " + numberOfMatches);
+  console.log("Matches from KMP: " + matchLocations.length);
 
-  return numberOfMatches;
+  return matchLocations.length;
 }
 
 async function GetData() {
@@ -53,59 +55,4 @@ async function GetComments(commentLink) {
   });
 
   return text.toLowerCase();
-}
-
-
-// start of KMP algorithm
-function KMP(pattern, text) {
-  let M = pattern.length;
-  let N = text.length;
-  let lps = new Array(M).fill(0);
-  let j = 0;
-  let i = 0;
-  let calculatedLps = CalculateLPSArray(pattern, M, lps);
-  let numberOfOccurences = 0;
-
-  while (i < N) {
-    if (pattern[j] == text[i]) {
-      i += 1;
-      j += 1;
-    }
-
-    if (j == M) {
-      //console.log("Found pattern at index " + (i - j));
-      numberOfOccurences += 1;
-      j = calculatedLps[j - 1];
-    } else if (i < N && pattern[j] != text[i]) {
-      if (j != 0) {
-        j = calculatedLps[j - 1];
-      } else {
-        i += 1;
-      }
-    }
-  }
-
-  return numberOfOccurences;
-}
-
-function CalculateLPSArray(pattern, M, lps) {
-  let size = 0;
-  let i = 1;
-
-  while (i < M) {
-    if (pattern[i] == pattern[size]) {
-      size += 1;
-      lps[i] = size;
-      i += 1;
-    } else {
-      if (size != 0) {
-        size = lps[size - 1];
-      } else {
-        lps[i] = 0;
-        i += 1;
-      }
-    }
-  }
-
-  return lps;
 }
